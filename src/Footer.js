9cb6b193844d1401ad1b65c20c7a5796598359b8
AkashQueraly <aakashqueraly@gmail.com>
2022-07-03 19:26:45 +0530

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>© 2022 Airbnb clone! By Akash</p>
+            <p>Privacy · Terms · Sitemap · Company Details</p>
+        </div>
+    )
+}
+
+export default Footer
